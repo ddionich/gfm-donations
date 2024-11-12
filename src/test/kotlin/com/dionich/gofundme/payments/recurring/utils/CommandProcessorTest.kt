1e35f720d42da9f9b6ddd8462708dfc2f15337e3
Dylan Dionich <dylan@dionich.com>
2024-11-12 00:37:50 -0300

    add test to check blank lines case

--- src/test/kotlin/com/dionich/gofundme/payments/recurring/utils/CommandProcessorTest.kt
+++ src/test/kotlin/com/dionich/gofundme/payments/recurring/utils/CommandProcessorTest.kt
@@ -33,6 +33,22 @@ class CommandProcessorTest {
     }
 
     @Test
+    fun `blank lines should work`() {
+        val list = mutableListOf(
+            "Add Donor Greg \$1000",
+            "Add Donor Janine \$100",
+            "Add Campaign SaveTheDogs",
+            "Add Campaign HelpTheKids",
+            "",
+            "",
+            "Donate Greg SaveTheDogs \$100",
+            "Donate Greg HelpTheKids \$200",
+            "Donate Janine SaveTheDogs \$50",
+        )
+        commandProcessor.processCommands(list)
+    }
+
+    @Test
     fun `exceeded limit should throw exception`() {
         val list = mutableListOf(
             "Add Donor Greg \$1000",
